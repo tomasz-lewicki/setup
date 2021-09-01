b428695fc4f994ab3ea4bac2207927afef668711
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-08-31 23:38:12 -0700

    Update base.sh

--- base.sh
+++ base.sh
@@ -2,32 +2,42 @@
 
 sudo apt-get update;
 sudo apt-get upgrade;
-sudo apt-get install git gparted python3-dev net-tools nmap tree ffmpeg -y;
+
+install_utils()
+{
+    sudo apt-get install git gparted python3-dev net-tools nmap tree ffmpeg vim curl tmux -y;
+}
+
+setup_shell()
+{
+    sudo apt-get install zsh -y;
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # Installs omyzsh
+
+    if [$SHELL != $(which zsh)]; then
+    chsh -s $(which zsh)
+    fi
+    # for some reason these aren't in default zsh PATH
+    echo "export PATH=$HOME/.local/bin:$HOME/bin:/usr/local/bin:$PATH" >> ~/.zshrc
+}
+
+setup_vim()
+{
+    wget https://raw.githubusercontent.com/tomek-l/dotfiles/master/.vimrc\?token\=AGHK36TT5WO65E4KQPF3S2DBG2GH4 -O  ~/.vimc
+}
+
+
+setup_git()
+{
+    git config --global user.email "t.w.lewicki@gmail.com" && git config --global user.name "Tomasz Lewicki"
+}
+
+setup_python()
+{
+    pip3 install Cython
+    pip3 install -r requirements.txt
+}
+
+# Not sure what needs this (pytorch?)
 sudo apt-get install libjpeg-dev libcanberra-dev libffi-dev -y;
-sudo apt-get install vim curl tmux -y;
-
-
-# Shell setup
-sudo apt-get install zsh -y;
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # Installs omyzsh
-
-if [$SHELL != $(which zsh)]; then
-chsh -s $(which zsh)
-fi
-
-
-# Setup vim
-wget https://raw.githubusercontent.com/tomek-l/dotfiles/master/.vimrc\?token\=AGHK36TT5WO65E4KQPF3S2DBG2GH4 -O  ~/.vimc
-
-# Python prerequisites
-pip3 install Cython
-pip3 install -r requirements.txt
-
-# Git setup
-git config --global user.email "t.w.lewicki@gmail.com" && git config --global user.name "Tomasz Lewicki"
-
-
-# $PATH setup
-echo "export PATH=$HOME/.local/bin:$HOME/bin:/usr/local/bin:$PATH" >> ~/.zshrc
 
 
