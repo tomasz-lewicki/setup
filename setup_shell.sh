330736aad301edb48c00904855dbf6f133487160
Tomasz Lewicki <tlewicki@covariant.ai>
2022-05-11 09:00:36 -0700

    Retire setup.sh in favor of narrower-scope setup_shell.sh

---
+++ setup_shell.sh
@@ -0,0 +1,17 @@
+# 1. Install zsh
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt-get install zsh -y;
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # do nothing. zsh is the default in macOS
+
+
+# 2. Install omyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# 3. Change shell if necessary
+if [$SHELL != $(which zsh)]; then
+chsh -s $(which zsh)
+fi
+
+# 4. Append $PATH
+echo "export PATH=$HOME/.local/bin:$HOME/bin:/usr/local/bin:$PATH" >> ~/.zshrc
