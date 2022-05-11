f0b13f5478b6d2e2286661e112bd5b865a79c51f
Tomasz Lewicki <tlewicki@covariant.ai>
2022-05-11 09:04:11 -0700

    Add setup_git.sh

--- setup.sh
+++ setup.sh
@@ -1,36 +1,10 @@
 # Apt
-
 sudo apt-get update;
 # sudo apt-get upgrade;
 
 install_utils()
 {
     sudo apt-get install git gparted python3-dev net-tools nmap tree ffmpeg vim curl tmux htop python3-pip -y;
-}
-
-setup_shell()
-{
-    # zsh + ohmyzsh
-    sudo apt-get install zsh -y;
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # Installs omyzsh
-
-    if [$SHELL != $(which zsh)]; then
-    chsh -s $(which zsh)
-    fi
-
-    # $PATH
-    echo "export PATH=$HOME/.local/bin:$HOME/bin:/usr/local/bin:$PATH" >> ~/.zshrc
-
-    # vim config
-    cp -r dotfiles/.vimrc ~/.vimrc
-
-    # tmux config
-    cp -r dotfiles/.tmux.conf ~/.tmux.conf
-
-setup_git()
-{
-    git config --global user.email "t.w.lewicki@gmail.com"; git config --global user.name "Tomasz Lewicki";
-    git config --global pull.ff only; git config --global init.defaultBranch main;
 }
 
 setup_python()
---
+++ setup_git.sh
@@ -0,0 +1,5 @@
+git config --global user.email "t.w.lewicki@gmail.com";
+git config --global user.email "tlewicki@covariant.ai";
+git config --global user.name "Tomasz Lewicki";
+git config --global pull.ff only;
+git config --global init.defaultBranch main;
