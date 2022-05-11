9b46738d83dbc9d4b38aefe677e4c04966112458
Tomasz Lewicki <tlewicki@covariant.ai>
2022-05-11 09:18:30 -0700

    Fix Syntax Error in shell script

--- setup_shell.sh
+++ setup_shell.sh
@@ -2,15 +2,15 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt-get install zsh -y;
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # do nothing. zsh is the default in macOS
+    : # do nothing. zsh is the default in macOS
 fi
 
 # 2. Install omyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # 3. Change shell if necessary
-if [$SHELL != $(which zsh)]; then
-chsh -s $(which zsh)
+if [[ $SHELL != "$(which zsh)" ]]; then
+    chsh -s "$(which zsh)"
 fi
 
 # 4. Append $PATH
