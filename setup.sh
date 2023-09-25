4ac3b70f93816e353cc03f6094ed07a655cb95aa
Tomasz Lewicki <t.w.lewicki@gmail.com>
2023-09-25 13:21:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,3 @@
-# Apt
-sudo apt-get update;
-# sudo apt-get upgrade;
-
-install_utils()
-{
-    sudo apt-get install git gparted python3-dev net-tools nmap tree ffmpeg vim curl tmux htop python3-pip -y;
-}
-
-setup_python()
-{
-    pip3 install --upgrade pip
-    pip3 install Cython
-    pip3 install -r requirements.txt
-}
-
-# Not sure what needs this (pytorch?)
-# sudo apt-get install libjpeg-dev libcanberra-dev libffi-dev -y;
-
-# install_utils;
-setup_vim;
-setup_git;
-# setup_python;
-setup_shell
+sudo apt-get install zsh -y;
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+chsh -s "$(which zsh)"; # if needed
