3c0151d0bdbf29384447e424371cc5637dd2b486
Tomasz Lewicki <t.w.lewicki@gmail.com>
2024-04-24 11:56:43 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
+wget https://raw.githubusercontent.com/tomek-l/dotfiles/master/copy-dotfiles.sh -O /tmp/copy-dotfiles.sh && bash /tmp/copy-dotfiles.sh
 sudo apt-get install zsh -y;
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 chsh -s "$(which zsh)"; # if needed
-wget https://raw.githubusercontent.com/tomek-l/dotfiles/master/copy-dotfiles.sh -O /tmp/copy-dotfiles.sh && bash /tmp/copy-dotfiles.sh
 
