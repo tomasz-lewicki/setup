72b3ea5567790c6be1304f12a3dbc847abb4f4b4
Tomasz Lewicki <t.w.lewicki@gmail.com>
2024-04-24 20:37:54 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ wget https://raw.githubusercontent.com/tomek-l/dotfiles/master/copy-dotfiles.sh 
 sudo apt-get install zsh -y;
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 chsh -s "$(which zsh)"; # if needed
-
+echo "export PATH="/home/tlewicki/.local/bin/:$PATH" >> ~/.zshrc
